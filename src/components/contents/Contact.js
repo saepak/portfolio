ccb5f68cda530ab884d7392152401ff00b7baa6e
saebyeolp <saebyeolpak@gmail.com>
2020-10-13 14:14:12 -0700

    added contact

--- src/components/contents/Contact.js
+++ src/components/contents/Contact.js
@@ -4,20 +4,20 @@ import { Container, Button } from '@material-ui/core'
 const Contact = () => {
     return (
         <Container className="subBox" maxWidth="md">
-            <div className="contactBox">
-                <h1 className="subTitle">Contact</h1>
-                <div className="contactBtnBox">
-                    <Button className="contactBtn">
-                        <span className="btnBg">E-mail</span>
-                    </Button>
-                    <Button className="contactBtn">
-                        <span className="btnBg">Github</span>
-                    </Button>
-                    <Button className="contactBtn">
-                        <span className="btnBg">Linkedin</span>
-                    </Button>
-                </div>
+
+            <h1 className="subTitle">Contact</h1>
+            <div className="contactBtnBox">
+                <Button className="contactBtn">
+                    <a href="mailto:saebyeolpak@gmail.com" className="btnBg">E-mail</a>
+                </Button>
+                <Button className="contactBtn">
+                    <a href="https://github.com/saebyeolp" target="_blank" className="btnBg">Github</a>
+                </Button>
+                <Button className="contactBtn">
+                    <a href="https://www.linkedin.com/in/saebyeolpak/" target="_blank" className="btnBg">Linkedin</a>
+                </Button>
             </div>
+
         </Container>
     )
 }
