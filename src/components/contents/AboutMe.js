279ec903c8aca47b9042971f38cd1f20ed253892
saebyeolp <saebyeolpak@gmail.com>
2020-11-12 20:40:44 -0800

    fixed

--- src/components/contents/AboutMe.js
+++ src/components/contents/AboutMe.js
@@ -7,7 +7,19 @@ const AboutMe = () => {
         <Container className="subBox" maxWidth="md">
             <div className="aboutMeBox">
                 <h1 className="subTitle">About Me</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dignissim lectus. Maecenas mi lacus, laoreet sed ante vitae, placerat faucibus ante. Aliquam erat volutpat. Praesent fringilla sem id lobortis commodo. Vestibulum purus sem, tristique ac bibendum et, pharetra quis dolor. Proin mollis, lectus id pharetra gravida, eros mi luctus enim, in luctus massa dolor sed mi. Donec rutrum sodales tristique. Cras convallis massa tincidunt metus feugiat sollicitudin. Morbi aliquet arcu sem, at mattis purus gravida eget. In nec ex fringilla, vulputate leo vitae, vulputate est. Cras luctus libero sed aliquam gravida. Nulla id velit a arcu finibus sodales. Duis felis leo, fringilla a nulla sed, fermentum ultricies eros. Aliquam quis bibendum velit. Fusce sodales nibh ac leo vestibulum, vel maximus est mattis.</p>
+                <div className="aboutMeDetail">
+                    <h3>Hi, I’m Saebyeol Pak, Vancouver based front-end developer. </h3>
+                    <p>Since beginning my journey as a developer, I've worked on web projects to meet customer satisfaction. I / I am..</p>
+
+                    <ul>
+                        <li>experienced in implementing responsive, cross-browser UI/UX designs using HTML and CSS.</li>
+                        <li>have hands-on experience in building web applications with React.js, Rest API and StyledComponents(Material-UI).</li>
+                        <li>familiar with using source control software (Git).</li>
+                        <li>able to work closely with designers and project managers.</li>
+                    </ul>
+                    <p>I'm a well-organised, collaborative person and I get excited about learning new technologies.
+I’m open for new opportunities! </p>
+                </div>
             </div>
         </Container>
     )
