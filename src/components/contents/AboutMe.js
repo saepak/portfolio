3e41b975756e66fbd55e9e04fd23b2622ec30103
saebyeolp <saebyeolpak@gmail.com>
2020-11-15 15:45:51 -0800

    fixed

--- src/components/contents/AboutMe.js
+++ src/components/contents/AboutMe.js
@@ -8,17 +8,15 @@ const AboutMe = () => {
             <div className="aboutMeBox">
                 <h1 className="subTitle">About Me</h1>
                 <div className="aboutMeDetail">
-                    <h3>Hi, I’m Saebyeol Pak, Vancouver based front-end developer. </h3>
-                    <p>Since beginning my journey as a developer, I've worked on web projects to meet customer satisfaction. I / I am..</p>
+                    <h3>Hi, I’m Saebyeol Pak</h3>
+                    <p>Detail-oriented front-end developer, friendly team player and self-motivated person. +3 years experience in building websites and web applications with a multitude of clients. Love collaboration and continually work on improving my skills. I / I am...</p>
 
                     <ul>
-                        <li>experienced in implementing responsive, cross-browser UI/UX designs using HTML and CSS.</li>
+                        <li>experienced in implementing responsive, cross-browser web projects using HTML5 and CSS3.</li>
                         <li>have hands-on experience in building web applications with React.js, Rest API and StyledComponents(Material-UI).</li>
                         <li>familiar with using source control software (Git).</li>
-                        <li>able to work closely with designers and project managers.</li>
                     </ul>
-                    <p>I'm a well-organised, collaborative person and I get excited about learning new technologies.
-I’m open for new opportunities! </p>
+                    <p>I look forward to speaking with you soon.</p>
                 </div>
             </div>
         </Container>
