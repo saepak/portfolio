1319db91af143448a1731023a68daa1134bdf0ca
saebyeolp <saebyeolpak@gmail.com>
2021-01-11 14:46:08 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-          {Expire()}
+          {/* {Expire()} */}
           <Header />
           <Nav />
           <Switch>
